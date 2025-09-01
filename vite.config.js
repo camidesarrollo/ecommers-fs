73a4efce90aff59f3c5846747e3bc61bb071ff9c
camidesarrollo <101568254+camidesarrollo@users.noreply.github.com>
2025-09-01 02:08:02 -0400

    git commit -m "Subiendo front Vue + Vite"

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import VueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import VueDevTools from 'vite-plugin-vue-devtools';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/ecommers-fs/', // 👈 muy importante para GitHub Pages
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
